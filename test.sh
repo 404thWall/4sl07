490ec001ce9f9eb3c87f25f8113632af5d14887a
Theo H <tt54.of@gmail.com>
2026-05-30 15:25:57 +0200

    Correcting the test script

--- test.sh
+++ test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-rm -rf ./tests/
+rm -rf ./tests/client_*/
+rm -rf ./tests/server/
+rm -rf ./tests/result/
 cargo build --release
 mkdir -p ./tests/server
 cp ./target/release/slr07 ./tests/server/
 cd ./tests/server/
-./slr07 server > ./server.log 2>&1 & PID_serv=$!
+./slr07 server 2>&1 > >(tee ./server.log) & PID_serv=$!
 cd ../..
 
 echo "Test (on a lancé $PID_serv)"
 echo "$PID_serv" > pids.txt
-
-
 
 N=3
 for i in $(seq 1 $N)
@@ -20,7 +20,7 @@ do
     mkdir ./tests/client_$i/
     cp ./target/release/slr07 ./tests/client_$i/
     cd ./tests/client_$i/
-    ./slr07 client $PORT 127.0.0.1 > ./client_$i.log 2>&1 & PID_client=$!
+    ./slr07 client $PORT 127.0.0.1 2>&1 > >(tee ./client_$i.log) & PID_client=$!
     echo "Client $i (PID: $PID_client) connecté au port $PORT"
     cd ../..
     echo "$PID_client" >> pids.txt
